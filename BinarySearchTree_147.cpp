89c218df2149dac3136c645fbb858125dc80df3f
Raraeva24 <rara.eva.ft21@mail.co.id>
2025-06-26 11:00:22 +0700

    membuat constructor untuk kelas node

---
+++ BinarySearchTree_147.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // constructor for the node class
+    Node()
+    {
+        leftchild = nullptr;
+        rightchild = nullptr;
+    }
+};
