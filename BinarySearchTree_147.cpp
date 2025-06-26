76c66980379daf7270ed9742304e3840df243270
Raraeva24 <rara.eva.ft21@mail.co.id>
2025-06-26 11:10:10 +0700

    jika data node baru lebih besar dairi parent, buat anak data sebelah kanan menjadi data node baru

--- BinarySearchTree_147.cpp
+++ BinarySearchTree_147.cpp
@@ -68,4 +68,13 @@ public:
             return;
         }
 
-       
+        // step 7: if the value in the data field of the new node is greater than that of the parent
+        else if (x > parent->info)
+        {
+            // 7a: make the right child of the parent point to the new node
+            parent->rightchild = newNode;
+
+            // 7b: exit
+            return;
+        }
+    }
